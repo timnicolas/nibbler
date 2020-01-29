6b239e39fe36ee3fef78b859eb95fda3eac5b25c
tnicolas42 <tnicolas@student.42.fr>
2020-01-29 17:35:07 +0100

    add rectangle and keyboard inputs in SFML

--- libs/nibblerSFML/srcs/NibblerSFML.cpp
+++ libs/nibblerSFML/srcs/NibblerSFML.cpp
@@ -29,17 +29,48 @@ bool NibblerSFML::init() {
 
 void NibblerSFML::updateInput() {
 	while (_win.pollEvent(_event)) {
-		if (_event.type == sf::Event::Closed)
-			input.quit = true;
+		switch (_event.type) {
+			// window closed
+			case sf::Event::Closed:
+				input.quit = true;
+				break;
+
+			// key pressed
+			case sf::Event::KeyPressed:
+				if (_event.key.code == sf::Keyboard::Escape)
+					input.quit = true;
+
+				else if (_event.key.code == sf::Keyboard::Up)
+					input.direction = ANibblerGui::Input::MOVE_UP;
+				else if (_event.key.code == sf::Keyboard::Down)
+					input.direction = ANibblerGui::Input::MOVE_DOWN;
+				else if (_event.key.code == sf::Keyboard::Left)
+					input.direction = ANibblerGui::Input::MOVE_LEFT;
+				else if (_event.key.code == sf::Keyboard::Right)
+					input.direction = ANibblerGui::Input::MOVE_RIGHT;
+
+
+				else if (_event.key.code == sf::Keyboard::Num1)
+					input.loadGuiID = 1;
+				else if (_event.key.code == sf::Keyboard::Num2)
+					input.loadGuiID = 2;
+				else if (_event.key.code == sf::Keyboard::Num3)
+					input.loadGuiID = 3;
+				break;
+
+			default:
+				break;
+		}
 	}
 }
 
 bool NibblerSFML::draw() {
 	_win.clear();
 
-	sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-	_win.draw(shape);
+	sf::RectangleShape rect(sf::Vector2f(10 + 100 * input.direction, 10 + 100 * (4 - input.direction)));
+	rect.setPosition(100, 100);
+    rect.setFillColor(sf::Color::Green);
+	_win.draw(rect);
 
 
 	_win.display();
