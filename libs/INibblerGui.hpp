37785b6148571aebe2f80a3a90f2d8a2ad152ce8
Ernest MARIN <emarin@student.42.fr>
2020-01-29 12:05:37 +0100

    basic NibblerGui interface

---
+++ libs/INibblerGui.hpp
@@ -0,0 +1,12 @@
+#ifndef INIBBLERGUI_HPP_
+#define INIBBLERGUI_HPP_
+
+class INibblerGui {
+	public:
+		virtual	bool	init() = 0;
+		virtual	bool	draw() = 0;
+
+	private:
+};
+
+#endif  // INIBBLERGUI_HPP_
